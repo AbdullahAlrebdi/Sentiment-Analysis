260442c1999b1283d2ba67b8826319c4bd660adf
Abdullah <abdullah.alrebdi7@gmail.com>
2024-03-25 11:04:25 +0300

    added tweets list

--- main.py
+++ main.py
@@ -8,4 +8,14 @@ with open("words_negative.txt") as f:
     negative_words = []
     for line in f:
         negative_words.append(line.rstrip())
+        
+        
+with open("tweets.txt") as f:
+    tweets = []
+    for line in f:
+        if line.rstrip() == '':
+            continue
+        tweets.append(line.rstrip())
+        
+
 
