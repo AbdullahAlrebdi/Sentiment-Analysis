240768ea0dfd315fef85da3bd138e5a331f58bdb
Your Name <you@example.com>
2024-03-25 10:57:49 +0300

    new ghanges

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ with open("words_positive.txt") as f:
     positive_words = []
     for line in f:
         positive_words.append(line.rstrip())
+
+
+with open("words_negative.txt") as f:
+    negative_words = []
+    for line in f:
+        negative_words.append(line.rstrip())
+
